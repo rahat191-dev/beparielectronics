b33cd4b4debeab5daae5c419044f48a1853bba70
Rahat Hossain <rahat191.dev@gmail.com>
2025-07-23 14:58:40 +0600

    added order form

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 setTimeout(() => {
   document.getElementById('welcome-screen').style.display = 'none';
   document.getElementById('main-content').style.display = 'block';
-}, 3000);
+}, 3000);6
+
+function goToForm(productName) {
+  // Local storage e product name save kore
+  localStorage.setItem("selectedProduct", productName);
+  // form.html e niye jay
+  window.location.href = "form.html";
+}
